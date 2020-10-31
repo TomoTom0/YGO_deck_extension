f0a5aba4e86a330d7b6c839f74279473636beeb4
Tomotom0 <arukuki4@outlook.jp>
2020-11-01 04:58:21 +0900

    auto commit

--- src/content_script.js
+++ src/content_script.js
@@ -1,5 +1,6 @@
 ﻿//----------------------
 const db_path="data/ygo_db_simple.csv";
+const btn_img_path="data/btn_black.png";
 let GLOBAL_df;
 
 
@@ -72,22 +73,108 @@ function exportAsYdk(){
     a.remove();
     URL.revokeObjectURL(url);
     alert("一部のカードが変換できませんでした。\n"+exceptions.join(" "));
-    console.log(content)
 }
 
+function obtain_deck_splited(data_array) {
+    const indexes = ["#main", "#extra", "!side"].map(d=>data_array.indexOf(d))
+    .concat([data_array.length]);
+    return [0,1,2].map(d=>data_array.slice(indexes[d]+1,indexes[d+1]));
+  };
 
+async function importFromYdk(){
+    const import_file=$("#button_importFromYdk_input").prop("files")[0];
+    const data_tmp=await import_file.text();
+    const data_array=data_tmp.split("\n");
+    const imported_ids=obtain_deck_splited(data_array);
+
+    const row_names=["monster", "spell", "trap", "extra", "side"];
+    const row_results=Array(5).fill({}).map(_ =>{ return {"names":[], "nums":[]}});
+    const df=GLOBAL_df;
+    let exceptions=[];
+    imported_ids.forEach((ids, ind)=>{
+        for (const id of Array.from(new Set(ids))){
+            if (!id) continue;
+            const name_tmp=df_filter(df, "Jap", ["id", id])[0];
+            const num_tmp=ids.filter(d=>d==id).length;
+            let types_tmp;
+            let main_row;
+            if (ind==0){
+                types_tmp=df_filter(df, "type", ["id", id])[0];
+                main_row=["monster", "spell", "trap"].map(d=>d.toUpperCase());
+            }
+            if (!name_tmp) exceptions.push(id, name_tmp);
+            else {
+                let row_index=ind+2;
+                if (ind==0){
+                    const row_type=main_row.filter(d=>types_tmp.split(" ").some(dd=>dd==d))[0];
+                    row_index=row_names.indexOf(row_type.toLowerCase());
+                }
+                row_results[row_index].names.push(name_tmp);
+                row_results[row_index].nums.push(num_tmp);
+            }
+        }
+    })
+    console.log(row_results, exceptions);
+
+    // deck_name
+    $("#dnm").val(import_file.name.replace(/\.ydk$/, ""));
+
+    for(const tab_ind of [...Array(4).keys()]){
+        const row_name=row_names[tab_ind];
+        const row_short_name=row_name.slice(0,2);
+        // reset
+        [...Array(60).keys()].forEach(ind2=>{
+            $(`#${row_name}_list #${row_short_name}nm_${ind2+1}`).val("");
+            $(`#${row_name}_list #${row_short_name}num_${ind2+1}`).val("");
+        })
+        if (row_results[tab_ind].names.length==0) continue;
+        const card_names=row_results[tab_ind].names;
+        const card_nums=row_results[tab_ind].nums;
+        card_names.forEach((name, ind2)=>{
+            $(`#${row_name}_list #${row_short_name}nm_${ind2+1}`).val(name);
+            $(`#${row_name}_list #${row_short_name}num_${ind2+1}`).val(card_nums[ind2]);
+        })
+        const sum_num=card_nums.reduce((acc,cur)=>acc+cur, 0);
+        [0,1].forEach(d=>{
+            const total_count=$(`.${row_name}_total:eq(${d})`);
+            total_count.empty();
+            total_count.append(sum_num);
+        })
+    }
+    const main_total=$(".main_total");
+    main_total.empty();
+    const main_total_num=[0,1,2].reduce((acc, cur)=> acc+Number($(`.main_count:eq(${cur})`).text()), 0);
+    main_total.append(main_total_num);
+}
 
 $(async function () {
-    const area=$("#reg_area");
-    const button=$("<button>", {class:"black_btn", type:"button", id:"button_exportAsYdk"}).append("<b>Export As .ydk</b>");
-    area.append(button);
+    const url_now=location.href;
+    if (url_now.indexOf("ope=2&")!=-1){
+        const area=$("#header_box .save");
+        const label=$("<label>", {for:"button_importFromYdk_input"});
+        const button=$("<a>", {class:"black_btn",type:"button" ,id:"button_importFromYdk", style:"position: relative;"})
+        .append("<b>インポート</b>");
+        const input_button=$("<input>", {type:"file", accpet: "text/*.ydk", style:"display: none;" ,id:"button_importFromYdk_input"});
+        button.append(input_button);
+        label.append(button);
+        area.append(label);
+    }
+    else if (/ope=1&|deck\.action\?cgid/.test(url_now) ){
+        const area=$("#header_box #button_place_edit");
+        const button=$("<a>", {class:"black_btn", id:"button_exportAsYdk", style:"position: relative;"})
+        .append("<b>エクスポート</b>");
+        area.append(button);
+    }
     const data=await fetch(chrome.runtime.getURL(db_path), {method: "GET"})
     .then(res=>res.text() )
     .then(data_tmp=>data_tmp.split("\n"))
     GLOBAL_df=CSV2Dic(data);
 
     $("#button_exportAsYdk").on("click", function(){
-        console.log("clicked");
         exportAsYdk();
+    })
+    
+    $("#button_importFromYdk").on("change", async function(){
+        await importFromYdk();
     })
 });
